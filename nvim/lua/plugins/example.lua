3e0c67c50eb8facff5eff20217deb1a89e94c94e
Łukasz <ywnwa96@gmail.com>
2023-09-30 16:00:10 +0200

    change karabiner and nvim

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -1,12 +1,7 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
 if true then return {
-{
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "koehler",
-    },
-  }
+  
 } end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
